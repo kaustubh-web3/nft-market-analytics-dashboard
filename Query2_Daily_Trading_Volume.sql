7869c89e318d4da686d565eac8cb06196a1da0d8
Kaustubh Waghmare <kaustubhanalyst23@gmail.com>
2026-08-03 15:41:58 +0530

    Update Query2_Daily_Trading_Volume.sql

--- Query2_Daily_Trading_Volume.sql
+++ Query2_Daily_Trading_Volume.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    block_date,
+    SUM(amount_usd) AS trading_volume_usd
+FROM nft.trades
+WHERE block_date >= CURRENT_DATE - INTERVAL '30' DAY
+GROUP BY block_date
+ORDER BY block_date;
