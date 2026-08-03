ab794141ad6fb51e1a9b9fffc78116247ba789e3
Kaustubh Waghmare <kaustubhanalyst23@gmail.com>
2026-08-03 15:44:34 +0530

    Update Query5_Top_Collections.sql

--- Query5_Top_Collections.sql
+++ Query5_Top_Collections.sql
@@ -1,1 +1,8 @@
-
+SELECT
+    project,
+    SUM(amount_usd) AS trading_volume_usd
+FROM nft.trades
+WHERE block_date >= CURRENT_DATE - INTERVAL '30' DAY
+GROUP BY project
+ORDER BY trading_volume_usd DESC
+LIMIT 10;
