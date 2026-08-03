240230f93d4bd1f1a97044e211d8dc26a1417b86
Kaustubh Waghmare <kaustubhanalyst23@gmail.com>
2026-08-03 15:43:35 +0530

    Update Query4_Daily_Unique_Sellers.sql

--- Query4_Daily_Unique_Sellers.sql
+++ Query4_Daily_Unique_Sellers.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    block_date,
+    COUNT(DISTINCT seller) AS unique_sellers
+FROM nft.trades
+WHERE block_date >= CURRENT_DATE - INTERVAL '30' DAY
+GROUP BY block_date
+ORDER BY block_date;
